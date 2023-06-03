31fb101e0a2f56ce1d77b241e865b316c2dd02d6
plaifah <plyfah08@gmail.com>
2023-06-03 22:49:26 +0700

    addnew

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # ใช้ include ในการเรียกใช้งาน urls.py ของแต่ละ app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('necsproject.urls')), # ใช้ include ในการเรียกใช้งาน urls.py ของแต่ละ app
 ]
