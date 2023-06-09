6df498e4482f64635101a442da999083f86fad63
plaifah <plyfah08@gmail.com>
2023-06-10 04:54:33 +0700

    add

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates'],
+        'DIRS': [ BASE_DIR / 'necsproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
