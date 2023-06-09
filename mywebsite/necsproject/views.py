3bd6682ef2961b05a9f23cf7a293a62d51a22851
plaifah <plyfah08@gmail.com>
2023-06-09 23:17:22 +0700

    add new

--- mywebsite/necsproject/views.py
+++ mywebsite/necsproject/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username)
+        print(password)
     return render(request ,"login.html")
 
 def jobAlert(request):
