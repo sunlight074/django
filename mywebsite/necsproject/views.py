7388fb38741f47b6af9cad4e43d41f19ae7d2192
worawut <worawut48@hotmail.co.th>
2023-06-19 21:36:07 +0700

    create new layout

--- mywebsite/necsproject/views.py
+++ mywebsite/necsproject/views.py
@@ -5,15 +5,16 @@ from django.contrib.auth import authenticate, login
 
 def login(request):
     if request.method == 'POST':
-        data = request.POST.copy()
-        username = data.get('username')
-        password = data.get('password')
-        print(username, password)
-        try:
-            user = authenticate(username=username, password=password)
-            login(request, user)
-        except:
-            return HttpResponse('Invalid Login') 
+        return render(request ,"dashboard.html")
+        # data = request.POST.copy()
+        # username = data.get('username')
+        # password = data.get('password')
+        # print(username, password)
+        # try:
+        #     user = authenticate(username=username, password=password)
+        #     login(request, user)
+        # except:
+        #     return HttpResponse('Invalid Login') 
         
     return render(request, 'login.html')
 
